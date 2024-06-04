54a61a20176dddbdff20075f924a3dae1bf341aa
Abhijit <udotavi@gmail.com>
2024-06-04 21:07:47 +0100

    Update: Added Bootstrap

--- jarvis-web-app/src/index.tsx
+++ jarvis-web-app/src/index.tsx
@@ -1,8 +1,15 @@
+// bootstrap imports
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
+import './index.css';
+
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
